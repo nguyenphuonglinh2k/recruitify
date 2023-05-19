04dc8c64bd7a9747461a02fc967d875d4510b2ab
nguyenphuonglinh2k <nguyenphuonglinh11102000@gmail.com>
2023-05-19 17:00:23 +0700

    update RatingOptionsModal

--- src/components/CommonRating.js
+++ src/components/CommonRating.js
@@ -1,30 +1,24 @@
 import React, { memo, useMemo } from "react";
 import PropTypes from "prop-types";
-import { StyleSheet, TouchableOpacity, View } from "react-native";
+import { StyleSheet, View } from "react-native";
 import { OutlineStarIcon, StarIcon } from "icons";
 import { AppConstant } from "const";
 import { COLORS } from "utils";
 
-const CommonRating = ({ color, value, readonly, style, ...otherProps }) => {
+const CommonRating = ({ color, value, style, ...otherProps }) => {
   const notFulfilStarTotal = useMemo(
     () => AppConstant.MAXIMUM_STAR_RATING - (value ?? 0),
     [value],
   );
 
-  const activeOpacity = useMemo(() => (readonly ? 1 : 0.5), [readonly]);
-
   return (
     <View style={[styles.root, style]} {...otherProps}>
       {Array.from(new Array(value)).map((_, index) => (
-        <TouchableOpacity key={index} activeOpacity={activeOpacity}>
-          <StarIcon color={color} />
-        </TouchableOpacity>
+        <StarIcon color={color} key={index} />
       ))}
       {notFulfilStarTotal > 0 &&
         Array.from(new Array(notFulfilStarTotal)).map((_, index) => (
-          <TouchableOpacity key={index} activeOpacity={activeOpacity}>
-            <OutlineStarIcon color={color} />
-          </TouchableOpacity>
+          <OutlineStarIcon color={color} key={index} />
         ))}
     </View>
   );
@@ -32,14 +26,12 @@ const CommonRating = ({ color, value, readonly, style, ...otherProps }) => {
 
 CommonRating.propTypes = {
   value: PropTypes.number,
-  readonly: PropTypes.bool,
   style: PropTypes.oneOfType([PropTypes.object, PropTypes.array]),
   color: PropTypes.string,
 };
 
 CommonRating.defaultProps = {
   value: 0,
-  readonly: true,
   color: COLORS.yellow,
 };
 
